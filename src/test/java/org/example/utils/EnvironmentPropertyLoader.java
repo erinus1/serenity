839110813cb88b9c08d7a9088d491fb395c3c209
erinus1 <sofiiabondarenko91@gmail.com>
2020-04-24 10:42:14 +0300

    EnvironmentSpecificConfiguration doesn't run tests

--- src/test/java/org/example/utils/EnvironmentPropertyLoader.java
+++ src/test/java/org/example/utils/EnvironmentPropertyLoader.java
@@ -1,21 +1,24 @@
 package org.example.utils;
 
 import net.serenitybdd.core.environment.EnvironmentSpecificConfiguration;
-import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.util.EnvironmentVariables;
 import net.thucydides.core.util.SystemEnvironmentVariables;
 import org.jbehave.core.steps.Steps;
-import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@RunWith(SerenityRunner.class)
 public class EnvironmentPropertyLoader extends Steps {
+    private static Logger log = LoggerFactory.getLogger(EnvironmentPropertyLoader.class);
+
+    private static EnvironmentPropertyLoader propertyLoader = null;
+
 
     public static String getProperty(final String propertyName) {
         EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
         EnvironmentSpecificConfiguration conf =  EnvironmentSpecificConfiguration.from(environmentVariables);
 
-        String v = conf.getProperty(propertyName);
+        String property = conf.getProperty(propertyName);
 
-        return v;
+        return property;
     }
 }
